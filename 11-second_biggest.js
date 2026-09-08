b466aaa81f0929faf28adb6a77f08ed40880f855
keffalyssa <keffalyssak@gmail.com>
2026-09-08 04:36:00 -0700

    Fix second biggest logic with Set to handle duplicates

--- 11-second_biggest.js
+++ 11-second_biggest.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-const args = process.argv.slice(2).map(Number);
-if (args.length <= 1) {
+const args = [...new Set(process.argv.slice(2).map(Number))];
+if (args.length < 2) {
   console.log(0);
 } else {
   args.sort((a, b) => a - b);
