f29c06be7ec2d517f9065b2a7aa3513c8551d675
Your Name <you@example.com>
2026-09-09 01:27:08 -0700

    Add factorial script

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+function factorial(n) {
+  const num = parseInt(n);
+
+  if (isNaN(num)) {
+    return 1;
+  }
+
+  if (num <= 1) {
+    return 1;
+  }
+
+  return num * factorial(num - 1);
+}
+
+const arg = process.argv[2];
+console.log(factorial(arg));
